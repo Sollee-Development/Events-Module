5e70fb53180e6c745abab9a6fca58fb09ff17d08
Richard <solleer@hotmail.com>
2019-03-08 22:29:19 -0500

    Add db setup sql
    Give defined method for setting up the table structure

---
+++ db-setup.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 08, 2019 at 07:00 PM
+-- Server version: 5.7.21
+-- PHP Version: 7.1.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prissy_pig`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) DEFAULT NULL,
+  `location` varchar(191) DEFAULT NULL,
+  `description` varchar(191) DEFAULT NULL,
+  `start_date` DATE DEFAULT NULL,
+  `start_time` varchar(191) DEFAULT NULL,
+  `end_date` DATE DEFAULT NULL,
+  `end_time` varchar(191) DEFAULT NULL,
+  `repeat_id` varchar(191) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `start_date asc` (`start_date`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `repeat_events`
+--
+
+DROP TABLE IF EXISTS `repeat_events`;
+CREATE TABLE IF NOT EXISTS `repeat_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `freq` varchar(191) DEFAULT NULL,
+  `interval_num` int(191) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `freq_id_interval_num` (`freq`,`id`,`interval_num`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
